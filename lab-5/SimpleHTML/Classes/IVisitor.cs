482f90b9dd76edd5e13c1a36124b9f741196e097
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-04-14 14:41:08 +0300

    Created Visitor interface.

---
+++ lab-5/SimpleHTML/Classes/IVisitor.cs
@@ -0,0 +1,10 @@
+﻿using SimpleHTML.Nodes;
+
+namespace SimpleHTML.Classes
+{
+    internal interface IVisitor
+    {
+        void Visit(LightElementNode element);
+        void Visit(LightTextNode textNode);
+    }
+}
