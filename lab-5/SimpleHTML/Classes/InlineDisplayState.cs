f5282ccf69a3b314fb3c2d297af55b034761e3be
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-04-14 14:34:07 +0300

    Added logic for State pattern.

---
+++ lab-5/SimpleHTML/Classes/InlineDisplayState.cs
@@ -0,0 +1,16 @@
+﻿using SimpleHTML.Nodes;
+
+namespace SimpleHTML.Classes
+{
+    internal class InlineDisplayState : IDisplayState
+    {
+        public void ApplyStyle(LightElementNode element)
+        {
+            if (!element.Classes.Contains("inline"))
+                element.Classes.Add("inline");
+
+            if (element.Classes.Contains("block"))
+                element.Classes.Remove("block");
+        }
+    }
+}
